efb5c4d08f4bd1999aa3cae5c636da76222ff0a9
JadeZhang4 <94367772+JadeZhang4@users.noreply.github.com>
2021-12-27 16:10:21 +0800

    Update just_test.py

--- just_test.py
+++ just_test.py
@@ -1,1 +1,10 @@
 print('there are nothing')
+amount = 230000
+rate = 0.05
+
+years = 50
+year = 1
+while year <= years:
+    amount = amount * (1 + rate)
+    print(f'第{year}年，本息合计{amount}元')
+    year = year + 1
